0d1a5cb5a0eccd3ef54d159f5dd17716f09d2b6e
Kurtt.z <zeankurtbalboa06@gmail.com>
2025-07-04 18:46:42 +0800

    commit

--- src/MyScript.server.lua
+++ src/MyScript.server.lua
@@ -1,7 +1,7 @@
 game.Players.PlayerAdded:Connect(function(player)
     print("Welcome, " .. player.Name .. "!")
     local message = Instance.new("Message")
-    message.Text = "Welcome mga gago sa Mount Tala, " .. player.Name .. "!"
+    message.Text = "Welcome gagong " .. player.Name .. " sa Mount Tala!"
     message.Parent = workspace
     task.delay(5, function()
         message:Destroy()
